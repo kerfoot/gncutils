63906b9c1a11ba7b88b6117831437ef8cc6d3d5d
John Kerfoot <johnkerfoot@gmail.com>
2018-04-09 10:45:43 -0400

    syntax fixes only

--- gncutils/yo/__init__.py
+++ gncutils/yo/__init__.py
@@ -9,6 +9,7 @@ from gncutils.yo.filters import default_profiles_filter
 
 logger = logging.getLogger(os.path.basename(__file__))
 
+
 def find_profiles(yo):
     """ Construct the depth time-series from a parsed dba['data'] array and find
     the profile minima and maxima.  The resulting indexed profiles are filtered
@@ -23,52 +24,56 @@ def find_profiles(yo):
     profile_times: a 2 column array containing the start and stop times of each
         indexed profile.  
     """
-    
-    profile_times = find_yo_extrema(yo[:,TIME_DIM], yo[:,DATA_DIM])
+
+    profile_times = find_yo_extrema(yo[:, TIME_DIM], yo[:, DATA_DIM])
     if len(profile_times) == 0:
         return profile_times
-    
+
     filter_profile_times = default_profiles_filter(yo, profile_times)
-    
+
     return filter_profile_times
-    
 
-def build_dba_yo(dba_reader):
 
-    sensor_names = [s['sensor_name'] for s in dba_reader['sensors']]
+def build_yo(glider_data):
+    """Return the yo (time-depth series) from dba_reader['data']."""
+    sensor_names = [s['sensor_name'] for s in glider_data['sensors']]
 
     # Find llat_time and llat_pressure
     if 'llat_time' not in sensor_names:
-        logger.warning('Parsed dba does not contain llat_time: {:s}'.format(dba_reader['file_metadata']['source_file']))
+        logger.warning('Parsed dba does not contain llat_time: {:s}'.format(glider_data['file_metadata']['source_file']))
         return
     if 'llat_pressure' not in sensor_names:
-        logger.warning('Parsed dba does not contain llat_pressure: {:s}'.format(dba_reader['file_metadata']['source_file']))
+        logger.warning(
+            'Parsed dba does not contain llat_pressure: {:s}'.format(glider_data['file_metadata']['source_file']))
         return
 
     ti = sensor_names.index('llat_time')
     pi = sensor_names.index('llat_pressure')
 
-    return dba_reader['data'][:, [ti, pi]]
+    return glider_data['data'][:, [ti, pi]]
 
 
-# def build_yo(data):
-#
-#     # Initialize yo array
-#     yo = np.empty((len(data),2)) * np.nan
-#
-#     row_count = -1
-#     for row in data:
-#
-#         row_count += 1
-#
-#         if 'drv_timestamp' in row:
-#             yo[row_count,TIME_DIM] = row['drv_timestamp']
-#
-#         if 'drv_depth' in row:
-#             yo[row_count,DATA_DIM] = row['drv_depth']
-#
-#     return yo
-        
+def slice_sensor_data(glider_data, sensors=[]):
+    """Return the sensor subsetted data array from dba_reader['data'] containing the sensors specified in sensors.  If
+    no sensors are specified, the yo (time-depth series) is returned."""
+    if not sensors:
+        return build_yo(glider_data)
+
+    sensor_names = [s['sensor_name'] for s in glider_data['sensors']]
+
+    num_rows = glider_data['data'].shape[0]
+    sensor_data = np.empty((num_rows, len(sensors))) * np.nan
+    for s in list(range(0, len(sensors))):
+        sensor = sensors[s]
+        if sensor not in sensor_names:
+            logger.warning('{:s} not found in glider data structure'.format(sensor))
+            continue
+
+        col = sensor_names.index(sensor)
+        sensor_data[:, s] = glider_data['data'][:, col]
+
+    return sensor_data
+
 
 def binarize_diff(data):
     data[data <= 0] = -1
@@ -79,10 +84,6 @@ def binarize_diff(data):
 def calculate_delta_depth(interp_data):
     delta_depth = np.diff(interp_data)
     delta_depth = binarize_diff(delta_depth)
-
-    #delta_depth = boxcar_smooth_dataset(delta_depth, 2)
-    
-    #delta_depth = binarize_diff(delta_depth)
 
     return delta_depth
 
@@ -112,7 +113,7 @@ def find_yo_extrema(timestamps, depth, tsint=10):
     Use filter_yo_extrema to remove invalid/incomplete profiles
     """
 
-    #validate_glider_args(timestamps, depth)
+    # validate_glider_args(timestamps, depth)
 
     est_data = np.column_stack((
         timestamps,
@@ -126,11 +127,11 @@ def find_yo_extrema(timestamps, depth, tsint=10):
     est_data = clean_dataset(est_data)
     if len(est_data) < 2:
         logger.debug('Skipping yo that contains < 2 rows')
-        return np.empty((0,2))
+        return np.empty((0, 2))
 
     # Create the fixed timestamp array from the min timestamp to the max timestamp
     # spaced by tsint intervals
-    ts = np.arange(est_data[:,0].min(), est_data[:,0].max(), tsint)
+    ts = np.arange(est_data[:, 0].min(), est_data[:, 0].max(), tsint)
     # Stretch estimated values for interpolation to span entire dataset
     interp_z = np.interp(
         ts,
@@ -140,34 +141,34 @@ def find_yo_extrema(timestamps, depth, tsint=10):
         right=est_data[-1, 1]
     )
 
-    filtered_z = boxcar_smooth_dataset(interp_z, int(tsint/2))
+    filtered_z = boxcar_smooth_dataset(interp_z, int(tsint / 2))
 
     delta_depth = calculate_delta_depth(filtered_z)
 
-    #interp_indices = np.argwhere(delta_depth == 0).flatten()
+    # interp_indices = np.argwhere(delta_depth == 0).flatten()
 
-    p_inds = np.empty((0,2))
+    p_inds = np.empty((0, 2))
     inflections = np.where(np.diff(delta_depth) != 0)[0]
 
     p_inds = np.append(p_inds, [[0, inflections[0]]], axis=0)
-    for p in range(len(inflections)-1):
-        p_inds = np.append(p_inds,[[inflections[p], inflections[p+1]]], axis=0)
-    p_inds = np.append(p_inds, [[inflections[-1], len(ts)-1]], axis=0)
+    for p in range(len(inflections) - 1):
+        p_inds = np.append(p_inds, [[inflections[p], inflections[p + 1]]], axis=0)
+    p_inds = np.append(p_inds, [[inflections[-1], len(ts) - 1]], axis=0)
 
-    #profile_timestamps = np.empty((0,2))
-    ts_window = tsint*2
-    
+    # profile_timestamps = np.empty((0,2))
+    ts_window = tsint * 2
+
     # Create orig GUTILS return value - lindemuth method
     # Initialize an nx3 numpy array of nans
-    profiled_dataset = np.full((len(timestamps),3), np.nan)
+    profiled_dataset = np.full((len(timestamps), 3), np.nan)
     # Replace TIME_DIM column with the original timestamps
     profiled_dataset[:, TIME_DIM] = timestamps
     # Replace DATA_DIM column with the original depths
     profiled_dataset[:, DATA_DIM] = depth
-    
+
     # Create Nx2 numpy array of profile start/stop times - kerfoot method
     profile_times = np.full((p_inds.shape[0], 2), np.nan)
-    
+
     # Start profile index
     profile_ind = 0
     # Iterate through the profile start/stop indices
@@ -177,32 +178,33 @@ def find_yo_extrema(timestamps, depth, tsint=10):
         # Profile end row
         p1 = int(p[1])
         # Find all rows in the original yo that fall between the interpolated timestamps
-        profile_i = np.flatnonzero(np.logical_and(profiled_dataset[:, TIME_DIM] >= ts[p0]-ts_window,profiled_dataset[:, TIME_DIM] <= ts[p1]+ts_window))
+        profile_i = np.flatnonzero(np.logical_and(profiled_dataset[:, TIME_DIM] >= ts[p0] - ts_window,
+                                                  profiled_dataset[:, TIME_DIM] <= ts[p1] + ts_window))
         # Slice out the profile
         pro = profiled_dataset[profile_i]
         # Find the row index corresponding to the minimum depth
         try:
-            min_i = np.nanargmin(pro[:,1])
+            min_i = np.nanargmin(pro[:, 1])
         except ValueError as e:
             logger.warning(e)
             continue
         # Find the row index corresponding to the maximum depth
         try:
-            max_i = np.nanargmax(pro[:,1])
+            max_i = np.nanargmax(pro[:, 1])
         except ValueError as e:
             logger.warning(e)
             continue
         # Sort the min/max indices in ascending order
         sorted_i = np.sort([min_i, max_i])
         # Set the profile index 
-        profiled_dataset[profile_i[sorted_i[0]]:profile_i[sorted_i[1]],2] = profile_ind
-        
+        profiled_dataset[profile_i[sorted_i[0]]:profile_i[sorted_i[1]], 2] = profile_ind
+
         # kerfoot method
-        profile_times[profile_ind,:] = [timestamps[profile_i[sorted_i[0]]], timestamps[profile_i[sorted_i[1]]]]
+        profile_times[profile_ind, :] = [timestamps[profile_i[sorted_i[0]]], timestamps[profile_i[sorted_i[1]]]]
         # Increment the profile index
         profile_ind += 1
-            
-        #profile_timestamps = np.append(profile_timestamps, [[est_data[profile_i[0][0],0], est_data[profile_i[0][-1],0]]], axis=0)
 
-    #return profiled_dataset
+        # profile_timestamps = np.append(profile_timestamps, [[est_data[profile_i[0][0],0], est_data[profile_i[0][-1],0]]], axis=0)
+
+    # return profiled_dataset
     return profile_times
