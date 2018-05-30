fe2bf5875132ecb3b8c8f67b8a7ce0b734e27755
Bob Fratantonio <bobfrat@gmail.com>
2018-05-30 12:08:47 -0400

    Update some docs, add LICENSE

--- gncutils/__init__.py
+++ gncutils/__init__.py
@@ -2,11 +2,13 @@
 import numpy as np
 from scipy.signal import boxcar, convolve
 
+__version__ = '0.1.0'
+
 
 def clean_dataset(dataset):
     """Remove any row in dataset for which one or more columns is np.nan
     """
-    
+
     # Get rid of NaNs
     dataset = dataset[~np.isnan(dataset[:, 1:]).any(axis=1), :]
 
